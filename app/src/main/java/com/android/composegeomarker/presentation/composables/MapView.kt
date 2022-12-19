24f75e35e2179cc5ca787674e58b453b6831b868
Karen <957458041@qq.com>
2022-12-19 17:55:24 +0800

    Creating a Custom JSON Map Styling

--- app/src/main/java/com/android/composegeomarker/presentation/composables/MapView.kt
+++ app/src/main/java/com/android/composegeomarker/presentation/composables/MapView.kt
@@ -37,13 +37,55 @@
 package com.android.composegeomarker.presentation.composables
 
 import android.content.Context
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import com.android.composegeomarker.R
+import com.google.android.gms.maps.model.CameraPosition
 import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MapStyleOptions
+import com.google.maps.android.compose.*
 
 @Composable
 fun MapView(context: Context, location: LatLng) {
 
-  // TODO add Camera Position State
+    val cameraPositionState = rememberCameraPositionState {
+        position = CameraPosition.fromLatLngZoom(location, 16f)
+    }
+    val infoWindowState = rememberMarkerState(position = location)
+    val mapProperties by remember {
+        mutableStateOf(
+            MapProperties(
+                mapStyleOptions = MapStyleOptions.loadRawResourceStyle(context, R.raw.map_style)
+            )
+        )
+    }
 
-  // TODO Add Google Map
+    GoogleMap(
+        modifier = Modifier.fillMaxSize(),
+        cameraPositionState = cameraPositionState,
+        properties = mapProperties
+    ) {
+        Marker(
+            state = MarkerState(position = location),
+        )
+        MarkerInfoWindow(
+            state = infoWindowState,
+            title = "My location",
+            snippet = "Location custom info window",
+            content = {
+                CustomInfoWindow(title = it.title, description = it.snippet)
+            }
+        )
+        Circle(
+            center = location,
+            fillColor = MaterialTheme.colorScheme.secondaryContainer,
+            strokeColor = MaterialTheme.colorScheme.secondaryContainer,
+            radius = 300.00
+        )
+    }
 }
--- app/src/main/java/com/android/composegeomarker/presentation/screens/MapScreenContent.kt
+++ app/src/main/java/com/android/composegeomarker/presentation/screens/MapScreenContent.kt
@@ -36,13 +36,50 @@
 
 package com.android.composegeomarker.presentation.screens
 
+import android.Manifest
 import androidx.compose.material3.SnackbarHostState
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
+import com.android.composegeomarker.R
+import com.android.composegeomarker.permissions.PermissionAction
+import com.android.composegeomarker.permissions.PermissionDialog
+import com.android.composegeomarker.presentation.composables.MapView
+import com.google.android.gms.maps.model.LatLng
+import kotlinx.coroutines.launch
 
 @Composable
 fun MapScreenContent(
     snackbarHostState: SnackbarHostState,
     fetchLocationUpdates: () -> Unit
 ) {
-  // TODO Add Permissions
+    val scope = rememberCoroutineScope()
+    val context = LocalContext.current
+    var showMap by rememberSaveable {
+        mutableStateOf(false)
+    }
+    PermissionDialog(
+        context = context,
+        permission = Manifest.permission.ACCESS_FINE_LOCATION,
+        permissionRationale = stringResource(id = R.string.permission_location_rationale),
+        snackbarHostState = snackbarHostState
+    ) { permissionAction ->
+        when (permissionAction) {
+            is PermissionAction.PermissionDenied -> {
+                showMap = false
+            }
+            is PermissionAction.PermissionGranted -> {
+                showMap = true
+                scope.launch {
+                    snackbarHostState.showSnackbar("Location permission granted!")
+                }
+                fetchLocationUpdates.invoke()
+            }
+        }
+    }
+    val currentLocation = LatLng(1.35, 103.87)
+    if (showMap) {
+        MapView(context, currentLocation)
+    }
 }
